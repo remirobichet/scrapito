08ec92e033caa77bf6e82bd8073c352864b7cd70
Rémi Robichet <remi.robichet@gmail.com>
2020-10-20 10:44:29 +0200

    :construction: Wip homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,49 @@
 <template>
   <div>
     <b-container class="mb-5">
-      <b-row class="my-3">
-        <b-col sm="3">
-          <label for="add-scrape">New scrape : </label>
-        </b-col>
-        <b-col cols="6" sm="6">
+      <b-form-group label-cols="4" label-cols-lg="2" label="New scrape">
+        <b-form-group
+          label-cols="4"
+          label-cols-lg="2"
+          label="Title : "
+          label-for="input-title"
+        >
+          <b-form-input id="input-title" ref="addScrapeTitle"></b-form-input>
+        </b-form-group>
+        <b-form-group
+          label-cols="4"
+          label-cols-lg="2"
+          label="Url : "
+          label-for="input-url"
+        >
           <b-form-input
-            id="add-scrape"
-            ref="addScrape"
-            type="text"
+            id="input-url"
+            type="url"
+            ref="addScrapeUrl"
           ></b-form-input>
-        </b-col>
-        <b-col cols="6" sm="3">
-          <b-button variant="outline-primary" @click="addScrape">Add</b-button>
-        </b-col>
-      </b-row>
+        </b-form-group>
+        <b-form-group
+          label-cols="4"
+          label-cols-lg="2"
+          label="Element target : "
+          label-for="input-element"
+        >
+          <b-form-input
+            id="input-element"
+            ref="addScrapeElement"
+          ></b-form-input>
+        </b-form-group>
+        <b-button variant="outline-primary" @click="addScrape">Add</b-button>
+      </b-form-group>
     </b-container>
     <b-container class="mb-5">
-      <b-list-group v-for="item in scrape" v-bind:key="item">
-        <b-list-group-item href="#">{{ item }}</b-list-group-item>
-      </b-list-group>
+      <b-card-group v-for="(item, idx) in scrape" v-bind:key="idx">
+        <b-card :title="item.title" class="my-3">
+          <b-card-text>{{ item.url }}</b-card-text>
+          <b-card-text>{{ item.element }}</b-card-text>
+          <b-link href="#" class="card-link">See results</b-link>
+        </b-card>
+      </b-card-group>
     </b-container>
   </div>
 </template>
@@ -39,11 +62,17 @@ export default Vue.extend({
   },
   methods: {
     addScrape() {
-      this.$store.commit(
-        'scrape/add',
-        ((this.$refs.addScrape as Vue).$el as HTMLInputElement).value
-      )
-      ;((this.$refs.addScrape as Vue).$el as HTMLInputElement).value = ''
+      const newScrape = {
+        title: ((this.$refs.addScrapeTitle as Vue).$el as HTMLInputElement)
+          .value,
+        url: ((this.$refs.addScrapeUrl as Vue).$el as HTMLInputElement).value,
+        element: ((this.$refs.addScrapeElement as Vue).$el as HTMLInputElement)
+          .value,
+      }
+      this.$store.commit('scrape/add', newScrape)
+      ;((this.$refs.addScrapeTitle as Vue).$el as HTMLInputElement).value = ''
+      ;((this.$refs.addScrapeUrl as Vue).$el as HTMLInputElement).value = ''
+      ;((this.$refs.addScrapeElement as Vue).$el as HTMLInputElement).value = ''
     },
   },
 })
