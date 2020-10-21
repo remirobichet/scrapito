3b90f3a4cae48b2e8911a1a7b6ed3ef7dffbc606
Rémi Robichet <remi.robichet@gmail.com>
2020-10-21 13:06:33 +0200

    :wrench: Update nuxt conf for axios url request

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,9 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    baseURL: '/',
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
