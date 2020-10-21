f633b87e670f683d7b491ad190278b0a3dcad7aa
Rémi Robichet <remi.robichet@gmail.com>
2020-10-21 13:04:50 +0200

    :sparkles: Implement scrape function

--- functions/scrape.js
+++ functions/scrape.js
@@ -1,29 +1,52 @@
 const Crawler = require('crawler')
 
-const scrape = () => {
+const HTTP_HEADERS = {
+  'Access-Control-Allow-Credentials': 'false',
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
+}
+
+const scrape = (url, element) => {
   let crawler = new Crawler()
   return new Promise((resolve, reject) => {
-    crawler.direct({
-      uri: 'https://www.leboncoin.fr/recherche/?text=table',
-      callback: function (error, response) {
+    var c = new Crawler({
+      maxConnections: 10,
+      // This will be called for each crawled page
+      callback: function (error, res, done) {
         if (error) {
           console.log(error)
         } else {
-          let $ = response.$
-          resolve($)
+          var $ = res.$
+          // $ is Cheerio by default
+          //a lean implementation of core jQuery designed specifically for the server
+          resolve($(element))
         }
+        done()
       },
     })
+
+    // Queue just one URL, with default callback
+    c.queue(url)
   })
 }
 
 exports.handler = async (event) => {
-  let $;
+  let $
 
-  $ = await scrape().then(res => res)
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+
+  const data = JSON.parse(event.body)
+  const url = data.url;
+  const element = data.element;
+
+  $ = await scrape(url, element).then((res) => res)
 
   return {
     statusCode: 200,
-    body: `${$('body')}`,
+    headers: HTTP_HEADERS,
+    body: `${$}`,
   }
 }
