3243fd8a068d61598d3ac605a48619a295856bd1
Rémi Robichet <remi.robichet@gmail.com>
2020-10-21 13:05:41 +0200

    :sparkles: Update view for scrape function reponse

--- pages/scrape/_id.vue
+++ pages/scrape/_id.vue
@@ -4,6 +4,19 @@
       <b-card :title="scrapeItem.title" class="my-3" v-if="scrapeItem">
         <b-card-text>{{ scrapeItem.url }}</b-card-text>
         <b-card-text>{{ scrapeItem.element }}</b-card-text>
+        <b-button @click="postScrape(scrapeItem.url, scrapeItem.element)"
+          >Load scrape</b-button
+        >
+      </b-card>
+    </b-container>
+    <b-container class="mb-5" v-if="response">
+      <b-card title="Result" class="my-3">
+        <b-card-text>{{ response }}</b-card-text>
+      </b-card>
+    </b-container>
+    <b-container class="mb-5" v-if="error">
+      <b-card title="Result" class="my-3">
+        <b-card-text>{{ error }}</b-card-text>
       </b-card>
     </b-container>
   </div>
@@ -16,12 +29,33 @@ export default Vue.extend({
   beforeCreate() {
     this.$store.commit('scrape/initialiseStore')
   },
+  data() {
+    return {
+      response: null,
+      error: null,
+    }
+  },
   computed: {
     scrapeItem() {
       if (this.$route.params.id) {
         return this.$store.getters['scrape/getScrapeById'](
           this.$route.params.id
         )
+      }
+    },
+  },
+  methods: {
+    async postScrape(url: string, element: string) {
+      try {
+        const res = await this.$axios.$post('/.netlify/functions/scrape', {
+          url,
+          element,
+        })
+        this.response = res
+        this.error = null
+      } catch (e) {
+        this.error = e.response
+        this.response = null
       }
     },
   },
