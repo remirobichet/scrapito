271c68626d5e717b77bc0cd2935f74d57120544d
Rémi Robichet <remi.robichet@gmail.com>
2020-10-22 20:03:35 +0200

    :sparkles: Add isJson mixin

--- mixins/global.js
+++ mixins/global.js
@@ -3,5 +3,13 @@ export default {
     uuid() {
       return Date.now().toString(31) + Math.random().toString(31).substr(2, 5)
     },
+    isJson(str) {
+      try {
+        JSON.parse(str)
+      } catch (e) {
+        return false
+      }
+      return true
+    },
   },
 }
